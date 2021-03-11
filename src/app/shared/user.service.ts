73cb8b72ec621adae806f3f364843aed9669b681
adamwroblewski.1993 <adamwroblewski.1993@gmail.com>
2021-03-11 11:39:45 +0100

    Create basic html form for register

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {User} from './user.model';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private appUrl = 'https://localhost:44369/api/user';
+  user: User = new User();
+
+  constructor(private http: HttpClient) {
+  }
+
+  public registerUser(): Observable<any> {
+    return this.http.post(this.appUrl, this.user);
+  }
 }
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from '../../shared/user.model';
+import {UserService} from '../../shared/user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public userService: UserService) {
+  }
 
   ngOnInit(): void {
   }
