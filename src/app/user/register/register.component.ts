78ca6db3e7df46c99b6304d7a9e2dbb829232ea2
adamwroblewski.1993 <adamwroblewski.1993@gmail.com>
2021-03-14 14:14:21 +0100

    Validate login and address email on register component

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../shared/user.service';
-import {NgForm} from '@angular/forms';
+import {NgForm, NgModel} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -17,6 +17,21 @@ export class RegisterComponent implements OnInit {
       .subscribe(res => {
         console.log(res);
       });
+  }
+
+  isLoginValid(model: NgModel): boolean {
+    if (model.touched && (model.model === undefined || model.model.length < 6)) {
+      return false;
+    }
+    return true;
+  }
+
+  isEmailValid(model: NgModel): boolean {
+    const regex = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$');
+    if (model.touched && !regex.test(model.model)) {
+      return false;
+    }
+    return true;
   }
 
   ngOnInit(): void {
